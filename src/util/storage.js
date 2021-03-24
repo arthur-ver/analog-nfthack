2a309558da78effa697b44cd5cf5a0da21775f1c
Arthur <19251998+arthur-ver@users.noreply.github.com>
2021-03-24 21:17:49 +0100

    branch init

--- src/util/storage.js
+++
@@ -1,8 +0,0 @@
-import {NFTStorage} from "nft.storage"
-const client = new NFTStorage({ token: process.env.VUE_APP_NFT_STORAGE })
-
-async function uploadToIPFS(file) {
-    return await client.storeBlob(file)
-}
-
-export {uploadToIPFS}
